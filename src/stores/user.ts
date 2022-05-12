3b5e312e0c5d7a332fd1e99048c925875b0839d1
ARGOUG <said.argoug@cgi.com>
2022-05-12 16:11:21 +0200

    add comment

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,46 +1,57 @@
 import { defineStore } from 'pinia';
 
+
 type User = {
     user: string,
     isAdmin: boolean
+}
+
+type UserState = {
+    user: User,
+    isUserConnected: boolean
 }
 
 const loginSuccess = { mail: "sadev" , password: '1234'}
 
 export const useUserStore = defineStore({
     id:"user",
-    state: ()=>({
-        user: null as User | null,
+    state: (): UserState =>({
+        user: {
+            user: '',
+            isAdmin: false
+        } ,
         isUserConnected: false
     }),
     actions: {
         login(mail: string , passw: string) {
             
             if(mail === loginSuccess.mail && passw === loginSuccess.password ) {
-                const obj = { user: mail ,isAdmin: true }
+                const obj = { user: mail , isAdmin: true }
                 this.user = obj
                 this.isUserConnected = true
+
+                //TODO 
+                //Side effect apres l'authentification
                 localStorage.setItem('userData' , JSON.stringify(obj))   
                 localStorage.getItem('userData') !== null ? this.$router.push('/') :  window.confirm('Error in log. Please retry')
             }
         },
         logout() {
-            localStorage.removeItem('userData')
-            this.user = null
-            this.isUserConnected = false
             let answer = window.confirm('Want to log out ?')
 
             if(answer) {
+                this.user = {
+                    user: '',
+                    isAdmin: false
+                }
+    
+                localStorage.removeItem('userData')
+                this.isUserConnected = false
+
                 this.$router.push('/')
             }
-        },
-        checkUserInLocalStorage(): boolean {
-            if(localStorage.getItem('userData')) {
-                this.user = JSON.parse(localStorage.getItem('userData'))
-                this.isUserConnected = true
-                return true
-            }
-            return false
-        },
+            
+    
+        }
     }
 })
